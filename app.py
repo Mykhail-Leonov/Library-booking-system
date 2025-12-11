e6946e8d98f26ea8a694d4bd450624ca3c8d11c5
Mykhail Leonov <myhail.leonov@gmail.com>
2025-12-11 15:17:08 +0000

    Created app.py with Flask with routes

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, send_from_directory
+from flask_cors import CORS
+
+app = Flask(__name__, static_folder="static")
+CORS(app)
+
+
+# HTML Routes
+
+@app.route("/")
+def serve_main():
+    return send_from_directory("static", "main.html")
+
+@app.route("/login")
+def serve_login():
+    return send_from_directory("static", "login.html")
+
+@app.route("/register")
+def serve_register():
+    return send_from_directory("static", "register.html")
+
+@app.route("/static/<path:path>")
+def serve_static(path):
+    return send_from_directory("static", path)
+
+
+# Run app
+
+if __name__ == "__main__":
+    app.run(debug=True)
